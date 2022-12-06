e85c87a4d4a03220d6ece5c3bf28be90dfe0d538
ViktoriaMelnyk <72021306+ViktoriaMelnyk@users.noreply.github.com>
2022-12-06 20:27:29 +0100

    share link button

---
+++ static/styles/main.js
@@ -0,0 +1,12 @@
+console.log('hello world')
+
+const copyBtn = [...document.getElementsByClassName('copy-btn')]
+console.log(copyBtn)
+
+copyBtn.forEach(btn=> btn.addEventListener('click', ()=>{
+    console.log('click')
+    const share = btn.getAttribute('share-link')
+    console.log(share)
+    navigator.clipboard.writeText(share)
+    btn.textContent = 'copied'
+}))
